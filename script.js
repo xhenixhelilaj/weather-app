d1691c65730082d985d4354e616f15f57328ef14
xhenixhelilaj <xhenixhelilaj@users.noreply.github.com>
2020-11-23 15:19:57 +0100

    First edit of the app

---
+++ script.js
@@ -0,0 +1,86 @@
+// 1 //
+
+let now = new Date();
+let p = document.querySelector("p");
+
+let date = now.getDate();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+p.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
+
+// 2 //
+
+// 3 //
+
+function convertToFahreineit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+let fahreneitLink = document.querySelector("#fahreneit");
+fahreneitLink.addEventListener("click", convertToFahreineit);
+
+// 4 //
+
+// 5 //
+function displayWeatherCondition(response) {
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#current-weather").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "4b314f77d77d39ad00e3f1ae01186f3d";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
