29bdf39bc7a11bc2cb26599640388c3f3765a423
xhenixhelilaj <xhenixhelilaj@users.noreply.github.com>
2020-11-29 15:00:50 +0100

    change js display day

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 // Day and Time //
 
-let now = new Date();
-let p = document.querySelector("p");
-let date = now.getDate();
-
-let days = [
-  "Sunday",
+function formatDate (timestamp){
+  let date = new Date (timestamp);
+  let p = document.querySelector("p");
+  let days = ["Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
-];
-let day = days[now.getDay()];
-let months = [
+  "Saturday"];
+  let day = days[date.getDay()];
+  let months = [
   "Jan",
   "Feb",
   "Mar",
@@ -27,19 +24,39 @@ let months = [
   "Oct",
   "Nov",
   "Dec"
-];
-let month = months[now.getMonth()];
+  ];
+  let month = months[now.getMonth()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0 ${hours}`
+  };
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`
+  };
 
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+  p.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
 }
 
-p.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
+function displayTemperature (response) {
+  let temperatureInput = document.querySelector ("#temperature");
+  let cityInput = document.querySelector ("#city");
+  let descriptionInput = document.querySelector ("#description");
+  let humidityInput = document.querySelector("#humidity");
+  let windInput = document.querySelector("#wind");
+  let dateInput = document.querySelector ("#date");
+
+  temperatureInput.innerHTML= Math.round(response.data.main.temp):
+  cityInput.innerHTML = response.data.name;
+  descriptionInput.innerHTML = response.data.weather[0].description;
+  humidityInput.innerHTML = response.data.main.humidity;
+  windInput.innerHTML = response.data.wind.speed;
+  date.innerHTML = formatDate (response.data.dt*1000)
+}
+
+let apiKey = "4b314f77d77d39ad00e3f1ae01186f3d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
 
 
 // Fahreneit //
@@ -48,12 +65,15 @@ function convertToFahreineit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   let temperature = temperatureElement.innerHTML;
+  let backroundImage = "https://image.shutterstock.com/image-photo/weather-forecast-background-climate-change-260nw-1124541077.jpg";
+  if (temperature > 7 === backroundImage)
   temperature = Number(temperature);
   temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
 }
 
 let fahreneitLink = document.querySelector("#fahreneit");
 fahreneitLink.addEventListener("click", convertToFahreineit);
+
 
 // Humidity and Wind //
 
@@ -81,3 +101,8 @@ function search(event) {
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+// change backround //
+
+
+
